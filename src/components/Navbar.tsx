c88eace9d807b8f236b63f03bef4e87534d83080
Zameerahmad01 <zameerahmad4877@gmail.com>
2024-09-09 21:25:34 +0500

    update navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,26 +8,32 @@ export default function Navbar({ className }: { className?: string }) {
   const [active, setActive] = useState<string | null>(null);
   return (
     <div
-      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+      className={cn("fixed top-5 inset-x-0 max-w-2xl mx-auto z-50", className)}
     >
       <Menu setActive={setActive}>
         <Link href={"/"}>
-        <MenuItem setActive={setActive} active={active} item="Home">
-        </MenuItem>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Home"
+          ></MenuItem>
         </Link>
 
         <MenuItem setActive={setActive} active={active} item="Our Courses">
-        <div className="flex flex-col space-y-4">
-        <HoveredLink href="/courses">All Courses</HoveredLink>
-        <HoveredLink href="/courses">Basic Music Theory</HoveredLink>
-        <HoveredLink href="/courses">Advance Composition</HoveredLink>
-        <HoveredLink href="/courses">Songwritting</HoveredLink>
-        <HoveredLink href="/courses">Music Production</HoveredLink>
-        </div>
+          <div className="flex flex-col space-y-4">
+            <HoveredLink href="/courses">All Courses</HoveredLink>
+            <HoveredLink href="/courses">Basic Music Theory</HoveredLink>
+            <HoveredLink href="/courses">Advance Composition</HoveredLink>
+            <HoveredLink href="/courses">Songwritting</HoveredLink>
+            <HoveredLink href="/courses">Music Production</HoveredLink>
+          </div>
         </MenuItem>
 
-        <MenuItem setActive={setActive} active={active} item="Contact Us">
-        </MenuItem>
+        <MenuItem
+          setActive={setActive}
+          active={active}
+          item="Contact Us"
+        ></MenuItem>
       </Menu>
     </div>
   );
